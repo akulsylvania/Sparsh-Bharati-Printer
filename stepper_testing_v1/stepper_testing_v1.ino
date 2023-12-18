05efa0a184bee9e6518c87abf27870b7cff5edfc
divyaratna joshi <1724akul@gmail.com>
2023-12-18 10:06:37 +0530

    Updated for movement in x y and servo

--- stepper_testing_v1/stepper_testing_v1.ino
+++ stepper_testing_v1/stepper_testing_v1.ino
@@ -1,36 +1,47 @@
-************************************************
+/************************************************
 NAME: SPARSH BHARATI PRINTER
 AUTHOR: DIVYARATNA JOSHI
 EMAIL: 1724akul@gmail.com
 ************************************************
 
-
 !!!IMPORTANT NOTE!!!
-FOR EVERY X MOVEMENT OF MOTX THERE MUST BE -X/2 MOVEMENT OF MOTY TO ACCOUNT
+FOR EVERY X MOVEMENT OF MOTX THERE MUST BE X/1.5 MOVEMENT OF MOTY TO ACCOUNT
 FOR TIMING BELT DRIFT
 
-************************************************
+************************************************/
 
 
 
 #include <AccelStepper.h>
+#include <Servo.h>
+
+
 
 //DEFINITIONS FOR MOTX
 #define STEPX 3
 #define DIRX 6
 #define EN 8
+#define servo 11
 
 //DEFINITIONS FOR MOTY
 #define STEPY 2
 #define DIRY 5
 
+//GLOBAL VARIABLES FOR X AND Y MOVEMENT
+//int x,y;
 
-//Create AccelStepper objects
+
+//Create AccelStepper and servo objects
 AccelStepper MOTX(AccelStepper::DRIVER ,STEPX, DIRX);
 AccelStepper MOTY(AccelStepper:: DRIVER ,STEPY,DIRY);
+Servo MOTZ;
 
 
 void setup() {
+
+
+// Attatch Servo
+  MOTZ.attach(servo);
 
 //Define pinout for enable
   pinMode(EN, OUTPUT);
@@ -47,25 +58,55 @@ void setup() {
 
 }
 
+
+
 void loop() {
 
 /*
 if(Serial.available()>0){
-  int step=Serial.parseInt();
+  int x=Serial.parseInt();
 
-  
- MOTX.moveTo(step);
- MOTX.runToPosition();
-
+  xMove(x);
  Serial.println("Completed");
  
 }
 */
-MOTX.move(1500);
 
-MOTY.move(750);
-MOTX.run();
-MOTY.run();
+yMove(100);
 
 
+
+
+
+
+}
+
+
+//SERVO ACTUATION
+void servAct()
+{
+  MOTZ.write(30);
+  MOTZ.write(0);
+  delay(200);
+  MOTZ.write(30);
+}
+
+
+//MOVEMENT IN X
+void xMove(int x)
+{
+  MOTX.move(x);
+  MOTY.move(x);
+  MOTX.run();
+  MOTY.run();
+}
+
+
+//MOVEMENT IN Y
+void yMove(int y)
+{
+  MOTY.move(y);
+  MOTX.move(-y);
+  MOTX.run();
+  MOTY.run();
 }
