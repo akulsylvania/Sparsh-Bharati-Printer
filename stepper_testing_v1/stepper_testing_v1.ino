d28d8e02dfdd1fd483cc55848dfe03c42979019a
divyaratna joshi <1724akul@gmail.com>
2023-12-18 06:27:22 +0530

    First test of AccelStepper library

--- stepper_testing_v1/stepper_testing_v1.ino
+++ stepper_testing_v1/stepper_testing_v1.ino
@@ -1,9 +1,62 @@
+#include <AccelStepper.h>
+
+//DEFINITIONS FOR MOTX
+#define STEPX 3
+#define DIRX 6
+#define EN 8
+
+//DEFINITIONS FOR MOTY
+#define STEPY 2
+#define DIRY 5
+
+
+//Create AccelStepper objects
+AccelStepper MOTX(AccelStepper::DRIVER ,STEPX, DIRX);
+AccelStepper MOTY(AccelStepper:: DRIVER ,STEPY,DIRY);
+
+
 void setup() {
-  // put your setup code here, to run once:
+
+//Define pinout for enable
+  pinMode(EN, OUTPUT);
+  digitalWrite(EN,LOW);
+
+  //Start Serial 
+  Serial.begin(9600);
+
+  //set max speed and accel
+  MOTX.setMaxSpeed(1000);
+  MOTY.setMaxSpeed(1000);
+  MOTX.setAcceleration(500);
+  MOTY.setAcceleration(500);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+/*
+if(Serial.available()>0){
+  int step=Serial.parseInt();
+
+  
+ MOTX.moveTo(step);
+ MOTX.runToPosition();
+
+ Serial.println("Completed");
+ 
+}
+*/
+MOTY.moveTo(500);
+MOTY.runToPosition();
+MOTX.moveTo(500);
+MOTX.runToPosition();
+
+
+delay(1000);
+MOTY.moveTo(-500);
+MOTY.runToPosition();
+MOTX.moveTo(-500);
+MOTX.runToPosition();
+delay(1000);
 
 }
